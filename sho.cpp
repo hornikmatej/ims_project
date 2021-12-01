bf4ffb08020ad03ba7d11412249bb2a823092dee
BicoVUT <f.brna123@gmail.com>
2021-12-01 10:21:37 +0100

    init

---
+++ sho.cpp
@@ -0,0 +1,10 @@
+/*
+Projekt: SHO vo vyrobe
+Autori: Filip Brna (xbrnaf00), Matej Hornik (xhorni20)
+Projekt simuluje výrobu rovadzaca vysokeho napatia UniGear spoločnosti ABB.
+Dalsie informacie a popis najdete v dokumentacii.
+jednotky času v systeme su minuty
+*/
+
+#include "simlib.h"
+#include "stdio.h"
