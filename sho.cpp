2979b36c654d25e826b1cd9cd7a741546e734ff8
BicoVUT <f.brna123@gmail.com>
2021-12-02 17:31:24 +0100

    uprava casov jednotlivych operacii

--- sho.cpp
+++ sho.cpp
@@ -9,12 +9,12 @@ jednotky času v systeme su minuty
 #include "simlib.h"
 #include <stdio.h>
 
-const int POC_STROJOV_MONTAZ_PANEL = 4;
-const int POC_STROJOV_MONTAZ_PODSTAVA = 5;
+const int POC_STROJOV_MONTAZ_PANEL = 3;
+const int POC_STROJOV_MONTAZ_PODSTAVA = 3;
 //const int POC_ZERIAV = 1;
-const int POC_STROJOV_MONTAZ_KABLE = 2;
-const int POC_STROJOV_MONTAZ_ZADNY_KRYT = 1;
-const int POC_STROJOV_MONTAZ_KLAPKY = 1;
+const int POC_STROJOV_MONTAZ_KABLE = 3;
+const int POC_STROJOV_MONTAZ_ZADNY_KRYT = 2;
+const int POC_STROJOV_MONTAZ_KLAPKY = 2;
 const int POC_STROJOV_MONTAZ_PREDNE_DVERE = 4;
 
 //stroje v zavode
@@ -29,7 +29,9 @@ Store Montaz_zadny_kryt("Z5", POC_STROJOV_MONTAZ_ZADNY_KRYT);
 Store Montaz_klapky("Z6", POC_STROJOV_MONTAZ_KLAPKY);
 Store Montaz_predne_dvere("Z7", POC_STROJOV_MONTAZ_PREDNE_DVERE);
 
-Histogram celk("Celkova doba v systeme", 0, 25, 12);
+Facility Otocna_panelov("Z8");
+
+Histogram celk("Celkova doba v systeme", 220, 20, 15);
 
 class Vyrobok : public Process
 {
@@ -43,37 +45,42 @@ class Vyrobok : public Process
         // Enter(predehrivac, 1);
         // Leave(predehrivac, 1);
         // WAIT
-        // Wait(Exponential(20));
+        // Wait(Normal(20));
         // Wait(1);
 
         // Z1
         Enter(Montaz_panel, 1);
-        Wait(Exponential(51.8));
+        Wait(Normal(51.8, 5));
         Leave(Montaz_panel, 1);
+
         // Z2
         Enter(Montaz_podstava, 1);
-        Wait(Exponential(75.5));
+        Wait(Normal(75.5, 5));
         Leave(Montaz_podstava, 1);
         // Z3
         Seize(Zeriav);
-        Wait(Uniform(8, 10));
+        Wait(Uniform(3, 5));
         Release(Zeriav);
         // Z4
         Enter(Montaz_kable, 1);
-        Wait(Exponential(9));
+        Wait(Normal(30.9, 7));
         Leave(Montaz_kable, 1);
         // Z5
         Enter(Montaz_zadny_kryt, 1);
-        Wait(Exponential(9));
+        Wait(Normal(9, 2));
         Leave(Montaz_zadny_kryt, 1);
         // Z6
         Enter(Montaz_klapky, 1);
-        Wait(Exponential(11.1));
+        Wait(Normal(11.1, 3));
         Leave(Montaz_klapky, 1);
         // Z7
         Enter(Montaz_predne_dvere, 1);
-        Wait(Exponential(61));
+        Wait(Normal(61, 6));
         Leave(Montaz_predne_dvere, 1);
+        // Z8
+        Seize(Otocna_panelov);
+        Wait(1);
+        Release(Otocna_panelov);
 
         celk(Time - prichod);
     } // Behavior
@@ -83,17 +90,23 @@ class Prichody : public Event
 {
     void Behavior()
     {
-        for (int i = 0; i < 3; i++)
+        int pocet = Montaz_panel.Free();
+        if (pocet != 0)
         {
-            (new Vyrobok)->Activate();
+            //printf("\n%0.0f minuta-", Time);
+            for (int i = 1; i < pocet + 1; i++)
+            {
+                //printf("%d.,  ", i);
+                (new Vyrobok)->Activate();
+            }
         }
-        Activate(Time + Exponential(51.8));
+        Activate(Time + 1);
     }
 };
 
 int main() // popis experimentu
 {
-    SetOutput("data2.dat");
+    SetOutput("data.dat");
     Init(0, 1000);
     (new Prichody)->Activate(); // start generatora
     Run();
